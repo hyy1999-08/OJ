c311816c15cdf7ef977970e851b2cd026cca7efa
hyy1999-08 <971742012@qq.com>
2024-01-18 22:22:28 +0800

    20240117 链表刷题

--- src/LinkedList/leetcode_19.java
+++ src/LinkedList/leetcode_19.java
@@ -22,11 +22,6 @@ public class leetcode_19 {
         }
     }
 
-    public static void main(String[] args) {
-
-
-    }
-
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (head == null) {
             return head;
